297c8d176b565b9b2c70520cb9d9f93208eceb28
Filipe Campos <filipefcampos2005@gmail.com>
2024-11-19 18:36:08 -0300

    Navbar implementada e projeto configurado

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+
+export class NavbarComponent implements OnInit {
+  currentRoute: string = ''; // Store the current route
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Subscribe to router events to get the current route
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects; // Get the final redirected URL
+        console.log('Current Route:', this.currentRoute);
+      }
+    });
+  }
+}
+
