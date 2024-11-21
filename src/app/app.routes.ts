2d14e24c9b98435856b18d266c711bbbff77b21b
Filipe Campos <filipefcampos2005@gmail.com>
2024-11-21 13:24:16 -0300

    Criação do formulário de cadastro e formatação dos campos de cpf e cnpj

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         title: 'Home'
+    },
+    {
+        path: 'cadastro',
+        component: CadastroComponent,
+        title: 'Cadastro'
     }
 ];
--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { FooterComponent } from '../footer/footer.component';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [ NavbarComponent, FooterComponent ],
+  imports: [],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
-export class CadastroComponent {
 
+export class CadastroComponent implements AfterViewInit {
+  
+  @ViewChild('campoCPF') campoCPF!: ElementRef<HTMLInputElement>;
+  @ViewChild('campoCNPJ') campoCNPJ!: ElementRef<HTMLInputElement>;
+
+  ngAfterViewInit(): void {
+    // Listener de formatação do CPF
+    this.campoCPF.nativeElement.addEventListener('keypress', () => {
+      const tamanhoCampo = this.campoCPF.nativeElement.value.length;
+
+      if (tamanhoCampo === 3 || tamanhoCampo === 7) {
+        this.campoCPF!.nativeElement.value += '.';
+      }
+      else if (tamanhoCampo === 11) {
+        this.campoCPF!.nativeElement.value += '-';
+      }
+    });
+    // Listener de formatação do CNPJ
+    this.campoCNPJ.nativeElement.addEventListener('keypress', () => {
+      const tamanhoCampo = this.campoCNPJ.nativeElement.value.length;
+
+      if (tamanhoCampo === 2 || tamanhoCampo === 6) {
+        this.campoCNPJ!.nativeElement.value += '.';
+      }
+      else if (tamanhoCampo === 10) {
+        this.campoCNPJ!.nativeElement.value += '/';
+      }
+      else if (tamanhoCampo === 15) {
+        this.campoCNPJ!.nativeElement.value += '-';
+      }
+    });
+  }
 }
