3738e75da16dc03c6cb24e1cf8210d19a9004154
Filipe Campos <filipefcampos2005@gmail.com>
2024-11-20 23:52:36 -0300

    Adicionando configuração básica do router (ainda não está funcionando)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TesteComponent } from './teste/teste.component';
 
 export const routes: Routes = [
     {
-        path: "",
-        component: HomeComponent
+        path: '',
+        component: HomeComponent,
+        title: 'Home'
+    },
+    {
+        path: 'teste',
+        component: TesteComponent,
+        title: 'Teste'
     }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ NavbarComponent, FooterComponent ],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
