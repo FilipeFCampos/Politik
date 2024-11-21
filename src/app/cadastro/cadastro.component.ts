99280887307811dc9fb3c7391015806de6a323f2
Filipe Campos <filipefcampos2005@gmail.com>
2024-11-21 20:28:32 -0300

    Adicionados os campos de endereço e informações do partido/candidato e código de formatação dividido em funções

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -11,33 +11,67 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 export class CadastroComponent implements AfterViewInit {
   
   @ViewChild('campoCPF') campoCPF!: ElementRef<HTMLInputElement>;
-  @ViewChild('campoCNPJ') campoCNPJ!: ElementRef<HTMLInputElement>;
+  @ViewChild('campoCandidatoCNPJ') campoCandidatoCNPJ!: ElementRef<HTMLInputElement>;
+  @ViewChild('campoCEP') campoCEP!: ElementRef<HTMLInputElement>;
+  @ViewChild('campoPartidoCNPJ') campoPartidoCNPJ!: ElementRef<HTMLInputElement>;
+
+  cargo = '';
 
   ngAfterViewInit(): void {
     // Listener de formatação do CPF
     this.campoCPF.nativeElement.addEventListener('keypress', () => {
       const tamanhoCampo = this.campoCPF.nativeElement.value.length;
 
-      if (tamanhoCampo === 3 || tamanhoCampo === 7) {
-        this.campoCPF!.nativeElement.value += '.';
-      }
-      else if (tamanhoCampo === 11) {
-        this.campoCPF!.nativeElement.value += '-';
-      }
+      formatCPF(tamanhoCampo, this.campoCPF);
     });
-    // Listener de formatação do CNPJ
-    this.campoCNPJ.nativeElement.addEventListener('keypress', () => {
-      const tamanhoCampo = this.campoCNPJ.nativeElement.value.length;
 
-      if (tamanhoCampo === 2 || tamanhoCampo === 6) {
-        this.campoCNPJ!.nativeElement.value += '.';
-      }
-      else if (tamanhoCampo === 10) {
-        this.campoCNPJ!.nativeElement.value += '/';
-      }
-      else if (tamanhoCampo === 15) {
-        this.campoCNPJ!.nativeElement.value += '-';
-      }
+    // Listener de formatação do CNPJ do candidato
+    this.campoCandidatoCNPJ.nativeElement.addEventListener('keypress', () => {
+      const tamanhoCampo = this.campoCandidatoCNPJ.nativeElement.value.length;
+
+      formatCNPJ(tamanhoCampo, this.campoCandidatoCNPJ);
     });
+
+    // Listener de formatação do CEP
+    this.campoCEP.nativeElement.addEventListener('keypress', () => {
+      const tamanhoCampo = this.campoCEP.nativeElement.value.length;
+
+      formatCEP(tamanhoCampo, this.campoCEP);
+    });
+    
+    // Listener de formatação do CNPJ do partido
+    this.campoPartidoCNPJ.nativeElement.addEventListener('keypress', () => {
+      const tamanhoCampo = this.campoPartidoCNPJ.nativeElement.value.length;
+
+      formatCNPJ(tamanhoCampo, this.campoPartidoCNPJ)
+    });
+  }
+}
+
+
+function formatCPF(tamanhoCampo: number, campoCPF: ElementRef<HTMLInputElement>):void {
+  if (tamanhoCampo === 3 || tamanhoCampo === 7) {
+    campoCPF!.nativeElement.value += '.';
+  }
+  else if (tamanhoCampo === 11) {
+    campoCPF!.nativeElement.value += '-';
+  }
+}
+
+function formatCNPJ(tamanhoCampo: number, campoCNPJ: ElementRef<HTMLInputElement>):void {
+  if (tamanhoCampo === 2 || tamanhoCampo === 6) {
+    campoCNPJ!.nativeElement.value += '.';
+  }
+  else if (tamanhoCampo === 10) {
+    campoCNPJ!.nativeElement.value += '/';
+  }
+  else if (tamanhoCampo === 15) {
+    campoCNPJ!.nativeElement.value += '-';
+  }
+}
+
+function formatCEP(tamanhoCampo: number, campoCEP: ElementRef<HTMLInputElement>):void {
+  if (tamanhoCampo === 5) {
+    campoCEP!.nativeElement.value += '-';
   }
 }
