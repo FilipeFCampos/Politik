7cc3525789fa27814237df2972ec6e3aacf2f4e0
Filipe Campos <filipefcampos2005@gmail.com>
2024-11-20 23:52:36 -0300

    Adicionando configuração básica do router (ainda não está funcionando)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { FooterComponent } from '../footer/footer.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent, CarouselComponent, CarouselComponent, CardComponent],
+  imports: [CarouselComponent, CarouselComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
