7037a6f7bf794a95cedd85389043800377f1ba09
Ulisses Damasceno <ulissesdamasceno@MacBook-de-Ulisses.local>
2024-11-21 19:07:41 -0300

    cenas imorais nas felicidades irrisorias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         title: 'Home'
+    },
+    {
+        path: 'feed',
+        component: FeedComponent,
+        title: 'Feed'
     }
 ];
