5dbb75b800906ece4c6a41382651bc5f49e784b9
André Lira <andrelira50@gmail.com>
2024-11-21 12:37:04 -0300

    adicionando a estrutura da home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CarouselComponent } from '../carousel/carousel.component';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ NavbarComponent, FooterComponent ],
+  imports: [NavbarComponent, FooterComponent, CarouselComponent, CarouselComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
