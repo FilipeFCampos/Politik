fa36569724ecf46131ac3bd9d2c58b1d7e375910
Filipe Campos <filipefcampos2005@gmail.com>
2024-11-21 00:03:29 -0300

    Router configurado (agora está funcionando)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { TesteComponent } from './teste/teste.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
         title: 'Home'
-    },
-    {
-        path: 'teste',
-        component: TesteComponent,
-        title: 'Teste'
     }
 ];
