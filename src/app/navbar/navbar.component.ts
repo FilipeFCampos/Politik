8653ab96cc98abbe5f8c1952462b8c9a98f2e761
Filipe Campos <filipefcampos2005@gmail.com>
2024-11-24 14:22:59 -0300

    Botão de alto contraste adicionado

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd, RouterLink } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ RouterLink, LoginComponent],
+  imports: [ RouterLink, LoginComponent ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
