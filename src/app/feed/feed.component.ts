56fde97781ea7d6463580951343538504a758f87
ulissesdamasceno7 <ufolador@gmail.com>
2024-11-25 21:16:00 -0300

    inicio do ts

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './feed.component.html',
-  styleUrl: './feed.component.scss'
+  styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent {
-
+  cards = [
+    { name: 'Fulano de Tal', role: 'Vereador', party: 'Partido', location: 'Natal/RN', image: 'images/canditado-generico.png' },
+    { name: 'Fulano de Tal', role: 'Prefeito', party: 'Partido', location: 'Natal/RN', image: 'images/canditado-generico.png' },
+    { name: 'Fulano de Tal', role: 'Governador', party: 'Partido', location: 'RN', image: 'images/canditado-generico.png' },
+    { name: 'Fulano de Tal', role: 'Dep. Estadual', party: 'Partido', location: 'RN', image: 'images/canditado-generico.png' },
+    { name: 'Fulano de Tal', role: 'Dep. Federal', party: 'Partido', location: 'RN', image: 'images/canditado-generico.png' },
+    { name: 'Fulano de Tal', role: 'Senador', party: 'Partido', location: 'RN', image: 'images/canditado-generico.png' }
+  ];
 }
