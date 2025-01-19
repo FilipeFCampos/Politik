6f6c8f5eeaf78c1735181f78faf9d1b10b98b9ba
André Lira <andrelira50@gmail.com>
2025-01-19 15:17:22 -0300

    adicionando minhas modificações antes de começar

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Candidato
+from .serializers import CandidatoSerializer
 
-# Create your views here.
+class CandidatoViewSet(ModelViewSet):
+    queryset = Candidato.objects.all()
+    serializer_class = CandidatoSerializer
