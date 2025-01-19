ff6f27f2aecb7ad480a3f47378cc291c9dbbf517
André Lira <andrelira50@gmail.com>
2025-01-19 15:57:51 -0300

    adicionando pesquisa

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 from .models import Candidato
 from .serializers import CandidatoSerializer
 
@@ -7,3 +7,6 @@ from .serializers import CandidatoSerializer
 class CandidatoViewSet(ModelViewSet):
     queryset = Candidato.objects.all()
     serializer_class = CandidatoSerializer
+
+    filter_backends = [SearchFilter]
+    search_fields = ['nome', 'partido', 'cidade', 'estado', 'numero']
