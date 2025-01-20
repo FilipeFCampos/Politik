17cbfb63d7eaa417a0ccfd9842e1d5d254ebfb46
FilipeFCampos <filipefcampos2005@gmail.com>
2025-01-20 14:39:07 -0300

    Funcionou

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,5 +10,5 @@ from django.urls import path
 from .views import submit_form
 
 urlpatterns = [
-    path('api/submit-form/', submit_form, name='submit_form'),
+    path('submit-form/', submit_form, name='submit_form'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -25,7 +25,7 @@ def submit_form(request):
             nome=data.get('nome'),
             nascimento=data.get('nascimento'),
             email=data.get('email'),
-            cpf=data.get('cpf'),
+            cpf=data.get('CPF'),
             endereco=data.get('endereço'),
             cidade=data.get('cidade'),
             bairro=data.get('bairro'),
@@ -47,7 +47,7 @@ def submit_form(request):
     return JsonResponse({"error": "Invalid request method"}, status=400)
 
 import json
-
+'''
 @csrf_exempt
 def submit_form(request):
     if request.method == "POST":
@@ -58,4 +58,4 @@ def submit_form(request):
             json.dump(all_data, json_file)
         return JsonResponse({"message": "Form submitted and saved as JSON!"}, status=201)
 
-    return JsonResponse({"error": "Invalid request method"}, status=400)
+    return JsonResponse({"error": "Invalid request method"}, status=400)'''
