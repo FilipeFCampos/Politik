8ac9984fdbb7ccf75793fa8bf72661734246855a
André Lira <andrelira50@gmail.com>
2025-01-20 16:07:09 -0300

    fazendo feed dinâmico e adicionando endpoint no modelo da api

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,6 +7,7 @@ class Candidato(models.Model):
     partido = models.CharField(max_length=50)
     cidade = models.CharField(max_length=50)
     estado = models.CharField(max_length=2)
+    endpoint = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.nome}-{self.cargo}-{self.partido}-{self.cidade}-{self.estado}"
